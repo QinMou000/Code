1b68d581aa78132ce1fed1fbe8195ebad0d42f65
QinMou <2830862261@qq.com>
2024-12-05 00:26:34 +0800

    跟进项目

--- 12_3/File.h
+++ 12_3/File.h
@@ -1,11 +1,21 @@
 #include<unistd.h>
 #include<stdlib.h>
 
+
+#define NONE_flush 1<<0
+#define LINE_flush 1<<1
+#define FULL_flush 1<<2
+
+#define MAX 1024
+
 struct FILE
 {
     int flag;
-    
-    char buffer[1024];
+    int flush_method;
+    int fileno;
+    char buffer[MAX];
+    int buffer_len;
 }
+
 
 my_openfile
