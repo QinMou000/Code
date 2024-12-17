27db6e03a9e2d5eea8daccd801c83a32690247ec
QinMou <2830862261@qq.com>
2024-12-17 23:12:57 +0800

    python语法

--- 12_15/test.py
+++ 12_15/test.py
@@ -49,3 +49,51 @@ print(list1)
 print(list2)
 
 # 元组和列表语法差不多，元组里的元素在定义时就定好了不能修改
+
+dict = {1:"汪钦",2:"汪如欽"}
+print(dict[1])
+print(2 in dict)
+
+dict[2] = "汪如丹"
+print(dict)
+
+# dict.pop(1)
+# dict.pop(2)
+# print(dict)
+
+for key in dict:
+    print(key,dict[key])
+
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+
+
+# 合法的 key 类型
+# 不是所有的类型都可以作为字典的 key. 
+# 字典本质上是一个 哈希表, 哈希表的 key 要求是 "可哈希的", 也就是可以计算出一个哈希值. 
+# 可以使用 hash 函数计算某个对象的哈希值. 
+# 但凡能够计算出哈希值的类型, 都可以作为字典的 key.
+print(hash(0))
+print(hash(3.14))
+print(hash('hello'))
+print(hash(True))
+print(hash(())) # () 是一个空的元组
+
+# print(hash([1, 2, 3]))
+# print(hash({'id':1}))
+
+f = open('d:/test.txt','w')
+f.write("hello world")
+f.close
+
+f = open('d:/test.txt','r')
+ret = f.read()
+print(ret)
+f.close()
+
+f = open('d:/test.txt', 'r')
+for line in f:
+    print(f'line = {line}')
+f.close()
+
