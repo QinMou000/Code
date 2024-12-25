4fd137f19a1f25faa63e5b3030f7ba99d700b008
Qin of Linux <2830862261@qq.com>
2024-12-25 20:35:17 +0800

    update

--- blog_web/app.py
+++ blog_web/app.py
@@ -48,17 +48,15 @@ def init_db():
     except sqlite3.Error as e:
         print(f"Database error: {e}")
 
+
 # 插入测试数据
 def insert_test_data():
     try:
         with sqlite3.connect("blog.db") as conn:
             conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', 
-                         ("C进阶哈希", "异常的概念异常处理机制允许程序中独⽴开发的部分能够在运⾏时就出\
-                          现的问题进⾏通信并做出相应的处理， 异常使得我们能够将问题的检测与解决问题的过程分开，\
-                          程序的⼀部分负责检测问题的出现，然后解决问题的任务传递给程序的另⼀部分，检测环节⽆须知\
-                          道问题的处理模块的所有细节。C语⾔主要通过错误码的形式处理错误，错误码本质就是对错误信\
-                          息进⾏分类编号，拿到错误码以后还要去查询错误信息，⽐较⿇烦。异常时抛出⼀个对象，这个对\
-                          象可以涵盖更全⾯的各种信息。"))
+                         ("技术博客", "https://blog.csdn.net/2301_80194476?spm=1000.2115.3001.5343"))
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', 
+                         ("码云", "https://gitee.com/wang-qin928"))
             conn.commit()
     except sqlite3.Error as e:
         print(f"Database error: {e}")
@@ -66,7 +64,7 @@ def insert_test_data():
 # 在应用启动时调用初始化函数
 with app.app_context():
     init_db()
-    insert_test_data()
+    #insert_test_data()
 
 @app.route("/")
 def index():
