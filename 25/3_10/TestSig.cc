38c0fc5476348b6ef6809541b21cdf383f42a947
Qin of Linux <2830862261@qq.com>
2025-03-13 23:53:45 +0800

    时钟信号

--- 25/3_10/TestSig.cc
+++ 25/3_10/TestSig.cc
@@ -22,34 +22,44 @@ void PrintPending(sigset_t &pending)
 
 int main()
 {
-    // for (int i = 1; i <= 31; i++)
-    // {
-    //     signal(i, handler);
-    // }
-
-    // signal(2, handler);
-
-    sigset_t block_set, old_set;
-    sigemptyset(&block_set);
-    sigemptyset(&old_set);
-    sigaddset(&block_set, SIGINT); // 屏蔽SIGNINT信号，但还没有设置到内核中
-
-    sigprocmask(SIG_BLOCK, &block_set, &old_set); // 设置到内核中
-
-    int cnt = 10;
-    while (1)
-    {
-        sigset_t pending;
-        sigpending(&pending);
-
-        PrintPending(pending);
-        cnt--;
-        if (cnt == 0)
-        {
-            std::cout << "解除对二号SIG_BLOCK信号的屏蔽" << std::endl;
-            sigprocmask(SIG_SETMASK, &old_set, &block_set);
-        }
-        sleep(1);
+    signal(SIGALRM, handler);
+    alarm(1);
+    while(1){
+        ;
     }
     return 0;
 }
+
+// int main()
+// {
+//     // for (int i = 1; i <= 31; i++)
+//     // {
+//     //     signal(i, handler);
+//     // }
+
+//     // signal(2, handler);
+
+//     sigset_t block_set, old_set;
+//     sigemptyset(&block_set);
+//     sigemptyset(&old_set);
+//     sigaddset(&block_set, SIGINT); // 屏蔽SIGNINT信号，但还没有设置到内核中
+
+//     sigprocmask(SIG_BLOCK, &block_set, &old_set); // 设置到内核中
+
+//     int cnt = 10;
+//     while (1)
+//     {
+//         sigset_t pending;
+//         sigpending(&pending);
+
+//         PrintPending(pending);
+//         cnt--;
+//         if (cnt == 0)
+//         {
+//             std::cout << "解除对二号SIG_BLOCK信号的屏蔽" << std::endl;
+//             sigprocmask(SIG_SETMASK, &old_set, &block_set);
+//         }
+//         sleep(1);
+//     }
+//     return 0;
+// }
