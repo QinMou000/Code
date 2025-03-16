c0546355e040858a4cc877c69a22911e6793af2b
QinMou <2830862261@qq.com>
2025-03-16 22:19:37 +0800

    信号图表

---
+++ 25/1_2/test.py
@@ -0,0 +1,57 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+
+from openai import OpenAI
+
+choose = input('please choose the api key: 1 for deepseek, 2 for openai: ')
+if choose == '1':
+    client = OpenAI(api_key="XXXX", base_url="https://api.deepseek.com")
+    while True:
+        que = input("please input your question:")
+        messages=[
+            {"role": "system", "content": "你是一个助手"},
+            {"role": "user", "content": que}
+        ]
+        response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=messages,
+        stream=False
+        )
+        print(response.choices[0].message.content)
+        messages.append(response.choices[0].message)
+elif choose == '2':
+    client = OpenAI(
+    api_key="XXXX"
+    )
+    while True:
+        que = input('please input your question:')
+        messages=[{"role": "user", "content": que}]
+        completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            store=True,
+            messages=messages, 
+        )
+        print(completion.choices[0].message.content)
+        messages.append(completion.choices[0].message)
+
+# from openai import OpenAI
+# client = OpenAI(api_key="sk-da819c7053db4723a5ae4c6dc6e7bb10", base_url="https://api.deepseek.com")
+
+# # Round 1
+# messages = [{"role": "user", "content": "What's the highest mountain in the world?"}]
+# response = client.chat.completions.create(
+#     model="deepseek-chat",
+#     messages=messages
+# )
+
+# messages.append(response.choices[0].message)
+# print(f"Messages Round 1: {messages}")
+
+# # Round 2
+# messages.append({"role": "user", "content": "What is the second?"})
+# response = client.chat.completions.create(
+#     model="deepseek-chat",
+#     messages=messages
+# )
+
+# messages.append(response.choices[0].message)
+# print(f"Messages Round 2: {messages}")
