bd523bcbc611219c785d4370691444acf7cf7711
Qin of Linux <2830862261@qq.com>
2025-03-27 22:01:00 +0800

    update

--- 25/Thread/main.cc
+++ 25/Thread/main.cc
@@ -1,8 +1,26 @@
-#include "thread.hpp"
+// #include "thread.hpp"
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <pthread.h>
+void *threadrun(void *args)
+{
+    int cnt = 5;
+    while (cnt--)
+    {
+        std::cout << "i am a new thread" << std::endl;
+        sleep(1);
+    }
+    int ret = 10;
+    return (void *)ret;
+}
 
 int main()
 {
-    std::cout << "hello world" << std::endl;
-
+    pthread_t tid;
+    pthread_create(&tid, nullptr, threadrun, nullptr);
+    int *ans;
+    pthread_join(tid, (void **)&ans);
+    std::cout << ans << std::endl;
     return 0;
 }
