7bb067d1905870fd8588e4e6ae808e0e498ac347
Qin of Linux <2830862261@qq.com>
2025-03-28 13:12:21 +0800

    线程库封装

--- 25/Thread/main.cc
+++ 25/Thread/main.cc
@@ -1,9 +1,10 @@
-// #include "thread.hpp"
+#include "thread.hpp"
 #include <iostream>
 #include <string>
 #include <unistd.h>
 #include <pthread.h>
-void *threadrun(void *args)
+
+void thread_run()
 {
     int cnt = 5;
     while (cnt--)
@@ -11,16 +12,47 @@ void *threadrun(void *args)
         std::cout << "i am a new thread" << std::endl;
         sleep(1);
     }
-    int ret = 10;
-    return (void *)ret;
 }
 
 int main()
 {
-    pthread_t tid;
-    pthread_create(&tid, nullptr, threadrun, nullptr);
-    int *ans;
-    pthread_join(tid, (void **)&ans);
-    std::cout << ans << std::endl;
+    std::string name("thread-1");
+    
+    Thread thread1(name, thread_run);
+
+    thread1.Detach();
+
+    thread1.Start();
+
+    int cnt = 6;
+    while(cnt--)
+    {
+        sleep(1);
+        std::cout << "主线程" << std::endl;
+    }
+
+
     return 0;
 }
+
+// void *threadrun(void *args)
+// {
+//     int cnt = 5;
+//     while (cnt--)
+//     {
+//         std::cout << "i am a new thread" << std::endl;
+//         sleep(1);
+//     }
+//     int ret = 10;
+//     return (void *)ret;
+// }
+
+// int main()
+// {
+//     pthread_t tid;
+//     pthread_create(&tid, nullptr, threadrun, nullptr);
+//     int *ans;
+//     pthread_join(tid, (void **)&ans);
+//     std::cout << ans << std::endl;
+//     return 0;
+// }
--- 25/Thread/thread.hpp
+++ 25/Thread/thread.hpp
@@ -2,23 +2,60 @@
 #include <string>
 #include <unistd.h>
 #include <pthread.h>
+#include <functional>
+
+using func_t = std::function<void()>;
 
 class Thread
 {
 private:
+    static void *routine(void *args)
+    {
+        Thread *self = static_cast<Thread *>(args);
+        self->_func();
+        return nullptr;
+    }
+
 public:
-    Thread()
-        : _tid(0)
+    Thread(std::string &name, func_t func)
+        : _tid(0),
+          _name(name),
+          _func(func),
+          _is_detach(false)
     {
     }
     bool Start()
     {
+        if (_tid == 0)
+        {
+            pthread_create(&_tid, nullptr, routine, this);
+            if (_is_detach = true)
+                pthread_detach(_tid);
+
+            return true;
+        }
+        return false;
     }
     bool Detach()
     {
+        if (_tid != 0)
+        {
+            pthread_detach(_tid);
+            return true;
+        }
+        else
+            _is_detach = true;
+
+        return false;
     }
     bool Join()
     {
+        if (_tid != 0)
+        {
+            pthread_join(_tid, nullptr);
+            return true;
+        }
+        return false;
     }
     ~Thread()
     {
@@ -27,4 +64,6 @@ public:
 private:
     pthread_t _tid;
     std::string _name;
+    func_t _func;
+    bool _is_detach;
 };
