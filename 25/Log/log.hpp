34622a60710d55400d606e905727664b12bedccf
Qin of Linux <2830862261@qq.com>
2025-04-01 23:53:52 +0800

    封装日志类

---
+++ 25/Log/log.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class Log
+{
+public:
+    Log()
+    {
+    }
+    ~Log()
+    {
+    }
+
+private:
+};
---
+++ 25/Log/main.cpp
@@ -0,0 +1,6 @@
+#include "log.hpp"
+
+int main()
+{
+    return 0;
+}
