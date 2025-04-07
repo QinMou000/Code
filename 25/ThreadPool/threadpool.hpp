67aad07d52e4cda5d32f9b84f00885b37969a494
Qin of Linux <2830862261@qq.com>
2025-04-07 21:57:15 +0800

    封装进程池

---
+++ 25/ThreadPool/threadpool.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "log.hpp"
+
+using namespace LogModule;
+
+class ThreadPool // 进程池
+{
+public:
+    ThreadPool()
+    {
+    }
+    ~ThreadPool()
+    {
+    }
+
+private:
+};
