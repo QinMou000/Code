9d5f70479e5cf85adea662cb2a66d7be0cd475ec
Qin of Linux <2830862261@qq.com>
2025-04-11 21:16:12 +0800

    UDP网络协议

--- 25/Udp/v1/UdpServer.hpp
+++ 25/Udp/v1/UdpServer.hpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <string>
+#include "log.hpp"
+#include <sys/socket.h> // 网络UDP四剑客
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+const uint16_t defaultport = 8080;
+
+using namespace LogModule;
 
 class UdpServer
 {
 
 public:
-    UdpServer()
+    UdpServer(uint16_t port)
+        : _port(defaultport),
+          _sockfd(-1)
+    {
+    }
+    void Init(std::string &ip)
+    {
+        // 1.创建网络文件
+        _sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        if (_sockfd < 0)
+        {
+            LOG(LogLevel::FATAL) << "socket error";
+            exit(1);
+        }
+
+        // 2.绑定网络信息
+    }
+    void Start() // 收到消息，echo 回去即可
     {
     }
     ~UdpServer()
@@ -13,4 +40,6 @@ public:
 
 private:
     int _sockfd;
+    uint16_t _port; // 本地端口号
+    std::string _ip; // 本地点分十进制ip
 };
