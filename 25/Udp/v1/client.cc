04dbe22f02c17f8b3d0e22b6c5b5d7f7456b879a
Qin of Linux <2830862261@qq.com>
2025-04-12 20:44:47 +0800

    daliy update

--- 25/Udp/v1/client.cc
+++ 25/Udp/v1/client.cc
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+#include "log.hpp"
+#include <sys/socket.h> // 网络UDP四剑客
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main()
 {
+    // 1.创建网络文件
+    _sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (_sockfd < 0)
+    {
+        LOG(LogLevel::FATAL) << "socket error";
+        exit(1);
+    }
+
     return 0;
 }
